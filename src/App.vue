e44fcfd925ad3f6e5a5133c92ef840eded8f777c
haozhuang <1340335269@qq.com>
2019-07-23 13:35:05 +0800

    第一次调试修改

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
     <div id="app">
         <router-view/>
-        <!--$route当前路由-->
         <FooterGuide v-show="$route.meta.showFooter"/>
     </div>
-
 </template>
 
 <script>
     import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 
     export default {
-        components: {
-            FooterGuide
-        }
+      components: {
+        FooterGuide
+      }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-    #app
+    .app
         width 100%
         height 100%
         background #f5f5f5
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   render: h =>h(App),
-  router
+  router,
+  store
 })
