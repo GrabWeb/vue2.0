84490e132305452610609ec866469f8042f28f42
haozhuang <1340335269@qq.com>
2019-07-25 14:39:25 +0800

    完成大部分项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
-/**
- * 路由器对象模块
+/*
+ 路由器对象模块
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import MSite from '../pages/MSite/MSite.vue'
-import Search from '../pages/Search/Search.vue'
-import Order from '../pages/Order/Order.vue'
-import Profile from '../pages/Profile/Profile.vue'
+
+// import MSite from '../pages/MSite/MSite.vue'
+// import Search from '../pages/Search/Search.vue'
+// import Order from '../pages/Order/Order.vue'
+// import Profile from '../pages/Profile/Profile.vue'
+
+const MSite = () => import('../pages/MSite/MSite.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
+
 import Login from '../pages/Login/Login.vue'
 import Shop from '../pages/Shop/Shop.vue'
 import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
@@ -18,13 +25,13 @@ import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
 Vue.use(VueRouter)
 
 export default new VueRouter({
-    //所有路由
+    // 所有路由
     routes: [
         {
             path: '/msite',
-            component: MSite,
+            component: MSite, // 返回路由组件的函数, 只有执行此函数才会加载路由组件, 这个函数在请求对应的路由路径时才会执行
             meta: {
-                showFooter: true 
+                showFooter: true
             }
         },
         {
@@ -49,12 +56,12 @@ export default new VueRouter({
             }
         },
         {
-            path: '/login',
-            component: Login
-        },
-        {
             path: '/',
             redirect: '/msite'
+        },
+        {
+            path: '/login',
+            component: Login
         },
         {
             path: '/shop',
